946a617ace3500e93f2a60fea8d67cd2754d5a62
hussam.ulhaq@yahoo.com <hussam.ulhaq@yahoo.com>
2022-06-23 04:43:46 -0700

    changes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import smtplib
 import mimetypes
 from io import BytesIO
 from email.message import EmailMessage
+import streamlit as st
 
 # Create message and set text content
 msg = EmailMessage()
@@ -22,9 +23,12 @@ def attach_bytesio_to_email(email, buf, filename):
     email.add_attachment(binary_data, maintype=maintype, subtype=subtype, filename=filename)
 
 # Attach files
-buf = BytesIO()
-buf.write(b"This is a test text")
-attach_bytesio_to_email(msg, buf, "test.txt")
+uploaded_files = st.file_uploader(accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    buf = BytesIO()
+    buf.write(bytes_data)
+    attach_bytesio_to_email(msg, buf, "test.txt")
 
 def send_mail_smtp(mail, host, username, password):
     s = smtplib.SMTP(host)
