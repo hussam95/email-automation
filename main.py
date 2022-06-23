773a13ede5ac1c5a22a6cad1e16e33fab735fb14
hussam.ulhaq@yahoo.com <hussam.ulhaq@yahoo.com>
2022-06-23 05:05:13 -0700

    attachments working gui done

--- main.py
+++ main.py
@@ -3,14 +3,86 @@ import mimetypes
 from io import BytesIO
 from email.message import EmailMessage
 import streamlit as st
+import os
+from PIL import Image
+import pandas as pd
+
+
+pwd = os.getcwd()
+
+# Locating Helper Files Setting Relative Paths to Avoid Hosting Issues
+img_path = os.path.join(pwd, "zem.jpg")
+csv_path = os.path.join(pwd, "email_record_ZEM.csv")
+
+# Landing Page Image Loading
+img = Image.open(img_path)
+
+# Landing Page Col Type Header
+col1, col2, col3 = st.columns(3)
+
+with col2:
+    st. markdown("<h1 style='text-align: center; color: Green;'>Zem Mail</h1>", unsafe_allow_html=True)
+    
+with col1:
+    st.image(img, width=100)
+
+with col3:
+    st.write("")
+
+# Email Groups
+email_groups = ["All", "Department Heads", "Admin", "Audit", "Engineering", "Executive", "Finance", "HR", "IT", "Marketing","Operations",
+ "Sales -Bahria Enclave", "Sales -GT Road", "Sales -Phase 08"]
+
+# User Selections/Inputs
+choice = st.sidebar.selectbox("Select Email Recepient Group", email_groups)
+email_sender = st.text_input("Enter Sender's Email")
+sender_pass = st.text_input("Enter Sender's password", type="password")
+subject = st.text_input("Enter Email's subject")
+email_content  = st.text_area("Email Body")
+
+
+# Email Data Connection
+email_data  = pd.read_csv(csv_path)
+
+# Sifitng Email dataframe w.r.t user selection 
+if choice == "Finance and Accounts":
+    recipients = email_data[email_data["Deparment"]=="Finance and Accounts"]["Email"].to_list()
+elif choice == "Audit Compliance":
+    recipients = email_data[email_data["Deparment"]=="Audit Compliance"]["Email"].to_list()
+elif choice == "Operations":
+    recipients = email_data[email_data["Deparment"]=="Operations"]["Email"].to_list()
+elif choice == "HR":
+    recipients = email_data[email_data["Deparment"]=="HR"]["Email"].to_list()
+elif choice == "Sales -GT Road":
+    recipients = email_data[email_data["Deparment"]=="Sales -GT Road"]["Email"].to_list()
+elif choice == "Marketing":
+    recipients = email_data[email_data["Deparment"]=="Marketing"]["Email"].to_list()
+elif choice == "Quality Assurance":
+    recipients = email_data[email_data["Deparment"]=="Quality Assurance"]["Email"].to_list()
+elif choice == "IT":
+    recipients = email_data[email_data["Deparment"]=="IT"]["Email"].to_list()
+elif choice == "Admin":
+    recipients = email_data[email_data["Deparment"]=="Admin"]["Email"].to_list()
+elif choice == "Procurement and Security":
+    recipients = email_data[email_data["Deparment"]=="Procurement and Security"]["Email"].to_list()
+elif choice == "Executive Office":
+    recipients = email_data[email_data["Deparment"]=="Executive Office"]["Email"].to_list()
+elif choice == "All":
+    recipients = email_data["Email"].to_list()
+elif choice == "Department Heads":
+    heads = ["Raja Shiraz Khalid","M. Mohsin Siddiqui", "M. Arslan Asharaf", "Sidra Zaheer",
+    "Zeshan Amjad Ali","Arif Nadeem","Amber Nosheen","Sami Ullah Abbasi", "Mazhar Iqbal","M. Rizwan Malik",
+    "Akbar Aziz Burqi","Kamil Qamar","Abdul Sattar", "Ismail Haiderr", "Asad Rasheed", "Ahmad Tohaeed Qasmi"]
+    recipients = email_data[email_data["Names"].isin(heads)]["Email"].to_list()
+
 
 # Create message and set text content
 msg = EmailMessage()
-msg['Subject'] = 'This email contains an attachment'
-msg['From'] = 'hussam.haq@zembuilders.com'
-msg['To'] = 'hussam.haq@zembuilders.com'
+msg['Subject'] = subject
+msg['From'] = email_sender
+msg['To'] = recipients
 # Set text content
-msg.set_content('Please see attached file')
+msg.set_content(email_content)
 
 def attach_bytesio_to_email(email, buf, filename):
     """Attach a file identified by filename, to an email message"""
@@ -37,4 +109,4 @@ def send_mail_smtp(mail, host, username, password):
     s.send_message(msg)
     s.quit()
 if st.button("Send Mail"):
-    send_mail_smtp(msg, 'mail.zembuilders.com', 'hussam.haq@zembuilders.com', 'zembuilders@1234')
+    send_mail_smtp(msg, 'mail.zembuilders.com', email_sender, sender_pass)
